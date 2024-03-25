eac5c2bc8b483c0db97b37e5d18e879c9630ea6e
Albina <n-albina@mail.ru>
2024-03-25 20:24:47 +0300

    style: исправить UserServiceDao

--- src/main/java/ru/yandex/practicum/filmorate/service/dao/FilmServiceDao.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/dao/FilmServiceDao.java
@@ -31,6 +31,7 @@ public class FilmServiceDao {
         String sql = "DELETE FROM LIKES WHERE FILMID = ? AND USERID = ?;";
         jdbcTemplate.update(sql, filmId, userId);
     }
+
     public Collection<Genre> getGenres() {
         String sql = "SELECT GENREID, NAME FROM GENRES;";
         return jdbcTemplate.query(sql, (rs, rowNum) -> makeGenre(rs));
@@ -62,6 +63,7 @@ public class FilmServiceDao {
     private Genre makeGenre(ResultSet rs) throws SQLException {
         return new Genre(rs.getInt("GENREID"), rs.getString("NAME"));
     }
+
     private RatingMPA makeMPA(ResultSet rs) throws SQLException {
         return new RatingMPA(rs.getInt("RATINGMPAID"), rs.getString("NAME"));
     }
